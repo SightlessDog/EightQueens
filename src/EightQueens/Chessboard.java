8ac03057c29e1b80c932571f566572014efa93ff
Elyess Eleuch <ely.369@outlook.com>
2020-06-10 23:09:01 +0200

    threats queens

--- src/EightQueens/Chessboard.java
+++ src/EightQueens/Chessboard.java
@@ -3,33 +3,75 @@ package EightQueens;
 import java.lang.reflect.Array;
 
 public class Chessboard {
-    int cols ;
-    int rows ;
-    String [][] array ;
+    int cols;
+    int rows;
+    String[][] array;
+    int[][] queens;
+    int i = 0;
+    boolean threat;
 
-    public Chessboard (int cols , int rows) {
-        this.cols = cols ;
-        this.rows = rows ;
+    public Chessboard(int cols, int rows) {
+        this.cols = cols;
+        this.rows = rows;
         this.array = new String[cols][rows];
+        this.queens = new int[cols][2];
     }
 
 
-
-    public void make () {
-        for (int i = 0 ; i < cols ; i++) {
-            for (int j = 0 ; j< rows ; j++){
-                array [i][j] = "[ ]";
+    public void make() {
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                array[i][j] = "[ ]";
             }
         }
     }
 
     public void printChess() {
-        for (int i = 0 ; i < cols ; i++) {
-            for (int j = 0 ; j< rows ; j++){
-                System.out.print(array [i][j]);
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(array[i][j]);
             }
             System.out.println();
         }
     }
 
+    public void addQueen(int col, int row) {
+        array[col][row] = "[0]";
+        queens[i][0] = col;
+        queens[i][1] = row;
+        i++;
+    }
+
+    public boolean threateningQueen() {
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (queens[i][1] == queens[j][1]) {
+                    threat = true;
+                    break;
+                } else if (queens[i][0] == queens[j][0]) {
+                    threat =true;
+                    break ;
+                } else if ((queens[i][1] == queens[j][1] + 1) && (queens[i][0] == queens[j][0] + 1)) {
+                    threat =true;
+                    break ;
+                } else if ((queens[i][1] == queens[j][1] - 1) && (queens[i][0] == queens[j][0] - 1)) {
+                    threat =true;
+                    break ;
+                } else if ((queens[i][1] == queens[j][1] + 1) && (queens[i][0] == queens[j][0] - 1)) {
+                    threat =true;
+                    break ;
+                } else if ((queens[i][1] == queens[j][1] - 1) && (queens[i][0] == queens[j][0] + 1)) {
+                    threat =true;
+                    break ;
+                } else {
+                    threat =false;
+                    break ;
+                }
+            }
+            if (threat ) {
+                break;
+            }
+        }
+       return threat ;
+    }
 }
--- src/EightQueens/Main.java
+++ src/EightQueens/Main.java
@@ -6,6 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Chessboard board = new Chessboard(8,8);
         board.make();
+        board.addQueen(3,4);
+        board.addQueen(0,4);
+        System.out.println(board.threateningQueen());
         board.printChess();
     }
 }
