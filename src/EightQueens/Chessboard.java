f328b29e33c8a160924d22ded3976d6197e42aff
MariusKM <m.morhard@hotmail.de>
2020-06-16 18:01:36 +0200

    Added Stack and recursive search

--- src/EightQueens/Chessboard.java
+++ src/EightQueens/Chessboard.java
@@ -1,22 +1,59 @@
 package EightQueens;
 
-import java.lang.reflect.Array;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Chessboard {
     int cols;
     int rows;
     String[][] array;
     int[][] queens;
-    int i = 0;
+    int queenCounter = 0;
     boolean threat;
+    public List<Queen> Queens = new ArrayList<>();
+
+
+    public class Queen {
+        public int cols, rows;
+        public boolean inThreat;
+
+        public Queen(int col, int row) {
+            this.cols = col;
+            this.rows = row;
+            this.inThreat = false;
+        }
+    }
+
+
+    public Point randomPos() {
+        return new Point((int) (Math.random() * 8), (int) (Math.random() * 8));
+    }
 
     public Chessboard(int cols, int rows) {
         this.cols = cols;
         this.rows = rows;
         this.array = new String[cols][rows];
         this.queens = new int[cols][2];
+        for (int i = 0; i < cols; i++) {
+            // set to a negative flag
+            queens[i][1] = -1;
+        }
+        make();
     }
 
+    public Chessboard(Chessboard copyFrom) {
+        this.cols = copyFrom.cols;
+        this.rows = copyFrom.rows;
+        this.array = deepCopy(copyFrom.array);
+        this.Queens = new ArrayList<>(copyFrom.Queens);
+        this.queens = deepCopy(copyFrom.queens);
+        this.queenCounter = copyFrom.queenCounter;
+
+    }
 
     public void make() {
         for (int i = 0; i < cols; i++) {
@@ -27,66 +64,73 @@ public class Chessboard {
     }
 
     public void printChess() {
-        for (int i = 0; i < cols; i++) {
-            for (int j = 0; j < rows; j++) {
-                System.out.print(array[i][j]);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(array[j][i]);
             }
             System.out.println();
         }
+        System.out.println("\n");
     }
 
     public void addQueen(int col, int row) {
+        Queens.add(new Queen(col, row));
         array[col][row] = "[0]";
-        queens[i][0] = col;
-        queens[i][1] = row;
-        i++;
+        queens[queenCounter][0] = col;
+        queens[queenCounter][1] = row;
+        queenCounter++;
     }
 
-    public boolean threateningQueen(String[][] array, int col, int row ) {
-        int i, j ;
+    public boolean isEmpty(int col, int row) {
+        if (array[col][row].contentEquals("[0]")) return false;
+        return true;
+    }
+
+
+    String[][] deepCopy(String[][] source) {
+        String[][] dest = new String[8][8];
+        for (int i = 0; i < 8; i++) {
+            dest[i] = Arrays.copyOf(source[i], source[i].length);
+        }
+        return dest;
+    }
+
+    int[][] deepCopy(int[][] source) {
+        int[][] dest = new int[8][8];
+        for (int i = 0; i < 8; i++) {
+            dest[i] = Arrays.copyOf(source[i], source[i].length);
+        }
+        return dest;
+    }
+
+// TODO: THIS ONLY WORKS FOR THE RIGHT SIDE DIAGONALLY. I ALREADY FIXED THE COLUMNS PLS FIX THE DIAGONAL
+    public boolean threateningQueen(String[][] array, int col, int row) {
+        int i, j;
         for (i = 0; i < cols; i++) {
-            // for (int j = 0 ; j < cols; j++) {
-              /*  if (queens[i][1] == queens[j][1] && array[queens[j][0]][queens[j][1]]=="[0]") {
-                    threat = true;
-                    break;
-                } else if (queens[i][0] == queens[j][0] && array[queens[j][0]][queens[j][1]]=="[0]") {
-                    threat =true;
-                    break ;
-                } else if ((queens[i][1] == (queens[j][1] + 1)) && (queens[i][0] == (queens[j][0] + 1)) && (array[queens[j][0]][queens[j][1]]=="[0]" && array[queens[i][0]][queens[i][1]]=="[0]")) {
-                    threat =true;
-                    break ;
-                } else if ((queens[i][1] == (queens[j][1] - 1)) && (queens[i][0] == (queens[j][0] - 1)) && (array[queens[j][0]][queens[j][1]]=="[0]" && array[queens[i][0]][queens[i][1]]=="[0]")) {
-                    threat =true;
-                    break ;
-                } else if ((queens[i][1] == (queens[j][1] + 1)) && (queens[i][0] == (queens[j][0] - 1))&& (array[queens[j][0]][queens[j][1]]=="[0]" && array[queens[i][0]][queens[i][1]]=="[0]")) {
-                    threat =true;
-                    break ;
-                } else if ((queens[i][1] == (queens[j][1] - 1)) && (queens[i][0] == (queens[j][0] + 1))&& (array[queens[j][0]][queens[j][1]]=="[0]" && array[queens[i][0]][queens[i][1]]=="[0]")) {
-                    threat =true;
-                    break ;
-                } else {
-                    threat =false;
-                } */
             //check if they have the same row
             if (queens[i][1] == row) {
-                return false;
+                return true;
+            }
+            if (queens[i][0] == col) {
+                return true;
             }
         }
-            //diagonal left side
-            for (i = col , j = row ; i>=0 && j>=0 ; i-- , j-- ) {
-                if (array [i][j] == "[0]"){
-                    return false ;
-                }
-            }
 
-            //diagonal left side down
-            for (i=col , j=row ; j<rows && i>=0 ; j++ ,i-- ) {
-                if (array[i][j] == "[0]"){
-                    return false ;
-                }
+        //diagonal left side
+        for (i = col, j = row; i >= 0 && j >= 0; i--, j--) {
+            if (array[i][j] == "[0]") {
+                return true;
             }
+        }
 
-            //otherwise, there is no threatening queen return true
-            return true ;
+        //diagonal left side down
+        for (i = col, j = row; j < rows && i >= 0; j++, i--) {
+            if (array[i][j] == "[0]") {
+                return true;
+            }
+        }
+
+        //otherwise, there is no threatening queen return false
+        return false;
     }
 }
--- src/EightQueens/Main.java
+++ src/EightQueens/Main.java
@@ -1,18 +1,77 @@
 package EightQueens;
 
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Main {
 
+    static Stack<Chessboard> states = new Stack();
+    static Chessboard board;
+    static int numQueens = 6;
+
     public static void main(String[] args) {
-        Chessboard board = new Chessboard(8,8);
-        board.make();
-        board.addQueen(4,0);
-        if (board.threateningQueen(board.array,1,1)){
-            board.addQueen(1,1);
+        Chessboard board = new Chessboard(8, 8);
+        // TODO THIS SHOWS THAT THE CHECK IS NOT WORKING. I CAN PLACE A QUEEN DIAGONALLY TO THE LEFT BUT NOT THE RIGHT
+        board.addQueen(4, 0);
+        if (!board.threateningQueen(board.array, 5, 1)) {
+            board.addQueen(5, 1);
         }
-        if (board.threateningQueen(board.array,3,2)) {
-            board.addQueen(3,2);
+        if (!board.threateningQueen(board.array, 3, 1)) {
+            board.addQueen(3, 1);
         }
         board.printChess();
+        search();
+    }
+
+
+
+    static void search() {
+        Chessboard currentState;
+        // if there are no states
+        if (states.isEmpty()) {
+            currentState = new Chessboard(8, 8);
+            board = new Chessboard(currentState);
+            states.push(currentState);
+        }
+        // get last one from stack
+        currentState = states.pop();
+        // termination condition
+        if (board.queenCounter >= numQueens) {
+            board.printChess();
+            return;
+        }
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+
+                // check if empty
+                if (currentState.isEmpty(x, y)) {
+                    // create copy
+                    Chessboard stateCopy = new Chessboard(currentState);
+                    // if it is empty add a queen
+                    // now check if the state has any threats
+                    if (!stateCopy.threateningQueen(stateCopy.array,x,y)){
+                        stateCopy.addQueen(x, y);
+                        // if it doesn't then save state
+                        states.push(stateCopy);
+                        board = new Chessboard(stateCopy);
+                        // continue search with this state
+                        search();
+                        return;
+                    }
+
+                }
+            }
+
+        }
+
+        // if I have searched all fields and there is nothing
+        if (!states.isEmpty()) {
+            search();
+        }
+
+        System.out.println("No possible option for "+numQueens+" Queens ");
     }
 }
